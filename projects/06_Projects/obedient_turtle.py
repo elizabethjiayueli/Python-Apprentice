3b5c5383fc88fc1c35f7369ed53ef7c074906ac4
elizabethjiayueli <moxueqin@hotmail.com>
2024-10-28 23:59:57 +0000

    Day 11: did 2 projects!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- projects/06_Projects/obedient_turtle.py
+++ projects/06_Projects/obedient_turtle.py
@@ -5,7 +5,31 @@ Make an obedient turtle that will obey commands to draw shapes.
 import turtle
 from guizero import App, Box, Text, TextBox, PushButton, ListBox, error
 
+t=turtle.Turtle()
 
+def circle():
+    t.circle(50, steps=50)
+
+def square():
+    for i in range(4):
+        t.forward(50+0*i)
+        t.right(90+0*i)
+
+
+def triangle():
+    for i in range(3):
+        t.forward(50+0*i)
+        t.right(120+0*i)
+
+command = input("Circle, square, or triangle? ALL LOWERCASE!")
+if command == "circle":
+    circle()
+elif command == "triangle":
+    triangle()
+elif command == "square":
+    square()
+else:
+    t.write("ERROR")
 # TODO)
 #   1. Create a turtle.
 #   2. Write 3 function definitions for drawing a square, triangle, and
--- projects/98_WIndows/high_low_game.py
+++ projects/98_WIndows/high_low_game.py
@@ -7,22 +7,30 @@ window = Tk()
 window.withdraw()
 
 # 1. Change this line to give you a random number between 1 - 100.
-random_num = random.randint(1, 10)
+random_num = random.randint(1, 101)
 
 # 2. Print out the random variable that you made in step #1
-
+print(random_num)
 # 3. Code a for loop to run steps 4-10, 10 times
+for i in range(10):
 
     # 4. Ask the user for a guess using a pop-up window, and save their response
-
+    guess = simpledialog.askinteger("HIGH LOW GAME", "Enter a number from 1-100")
     # 5. If the guess is correct
         # 6. Win. Use 'sys.exit(0)' to end the program
-
+    if guess == random_num:
+        sys.exit(0)
+    elif guess > random_num:
+        messagebox.showinfo("HIGH LOW GAME", "It's too high")
     # 7. if the guess is high
         # 8. Tell them it's too high
     # 9. Else if the guess is low
         # 10. Tell them it's too low
+    elif guess < random_num:
+        messagebox.showinfo("HIGH LOW GAME", "It's too low")
+    else:
+        pass
 
 #11. Outside of the loop, tell the user they lost
-
+messagebox.showinfo("HIGH LOW GAME", "YOU LOST, TRY AGAIN")
 window.mainloop()
